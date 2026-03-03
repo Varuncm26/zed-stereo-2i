6c92e09006ef87acbbfab86b0b283ac0f70fcc18
Varuncm26 <varun.cdmb@gmail.com>
2026-03-03 16:38:36 +0530

    Update uart_sample.cpp

--- uart_sample.cpp
+++ uart_sample.cpp
@@ -1,65 +1,75 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <pthread.h>
 #include <termios.h>
-#include <poll.h>
 #include "uart.h"
 
-// Helper to set Jetson terminal to raw mode so we catch keys instantly
-void set_conio_terminal_mode(struct termios *orig) {
-    struct termios new_termios;
-    tcgetattr(0, orig);
-    new_termios = *orig;
-    cfmakeraw(&new_termios);
-    tcsetattr(0, TCSANOW, &new_termios);
+int serial_fd1;
+pthread_mutex_t uart_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Function to put terminal in raw mode so getchar() is instant
+void set_raw_mode(struct termios *orig) {
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, orig);
+    raw = *orig;
+    raw.c_lflag &= ~(ICANON | ECHO); // Disable buffering and local echo
+    tcsetattr(STDIN_FILENO, TCSANOW, &raw);
 }
 
-void reset_terminal_mode(struct termios *orig) {
-    tcsetattr(0, TCSANOW, orig);
+void reset_mode(struct termios *orig) {
+    tcsetattr(STDIN_FILENO, TCSANOW, orig);
+}
+
+int CreatePacket(char key, unsigned char *buffer) {
+    buffer[0] = 0xAA;      // Start byte
+    buffer[1] = (unsigned char)key; // Key pressed
+    buffer[2] = 0x55;      // End byte
+    return 3; 
+}
+
+void *send_thread(void *arg) {
+    unsigned char txBuffer[10];
+    int len;
+    char key;
+
+    printf("UART Active. Press any key to send packet... (Ctrl+C to exit)\n\r");
+
+    while (1) {
+        key = getchar(); // Now returns immediately due to raw mode
+
+        pthread_mutex_lock(&uart_mutex);
+        len = CreatePacket(key, txBuffer);
+        int write_status = write(serial_fd1, txBuffer, len);
+
+        if (write_status > 0) {
+            // Use \r for clean printing in raw mode
+            printf("Sent Packet: [0xAA] [%c] [0x55]\n\r", key);
+        } else {
+            perror("UART write failed");
+        }
+        pthread_mutex_unlock(&uart_mutex);
+    }
+    return NULL;
 }
 
 int main() {
     struct termios orig_termios;
-    int uart_fd = InitUart(B115200);
-    
-    if (uart_fd < 0) return 1;
+    pthread_t tx_thread;
 
-    printf("Starting Chat. Type locally to send. Press Ctrl+C to quit.\n\r");
-
-    // Set local keyboard to raw mode
-    set_conio_terminal_mode(&orig_termios);
-
-    struct pollfd fds[2];
-    fds[0].fd = 0;          // Standard Input (Keyboard)
-    fds[0].events = POLLIN;
-    fds[1].fd = uart_fd;    // UART Port
-    fds[1].events = POLLIN;
-
-    char buf[256];
-    while (1) {
-        // Wait for either the keyboard or the UART to have data
-        if (poll(fds, 2, -1) > 0) {
-            
-            // 1. If Keyboard has data -> Write to UART
-            if (fds[0].revents & POLLIN) {
-                int n = read(0, buf, sizeof(buf));
-                if (n > 0) {
-                    // Check for Ctrl+C (ASCII 3) to exit raw mode gracefully
-                    if (buf[0] == 3) break; 
-                    write(uart_fd, buf, n);
-                }
-            }
-
-            // 2. If UART has data -> Write to Screen
-            if (fds[1].revents & POLLIN) {
-                int n = read(uart_fd, buf, sizeof(buf));
-                if (n > 0) {
-                    write(1, buf, n); // Write directly to stdout
-                }
-            }
-        }
+    // 1. Use your existing InitUart function
+    serial_fd1 = InitUart(B115200); 
+    if (serial_fd1 < 0) {
+        return -1;
     }
 
-    reset_terminal_mode(&orig_termios);
-    close(uart_fd);
+    // 2. Set Jetson terminal to raw mode for instant keypresses
+    set_raw_mode(&orig_termios);
+
+    // 3. Start the thread
+    pthread_create(&tx_thread, NULL, send_thread, NULL);
+    pthread_join(tx_thread, NULL);
+
+    // Cleanup
+    reset_mode(&orig_termios);
+    close(serial_fd1);
     return 0;
 }
